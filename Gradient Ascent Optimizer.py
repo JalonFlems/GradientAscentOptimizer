466ee7d695279dc74ea0e8cf19b99959cc5a1697
Micah Flemming <micah.flemming@utoronto.ca>
2019-09-14 04:16:30 -0400

    Add function for Gradient Ascent

--- Gradient Ascent Optimizer.py
+++ Gradient Ascent Optimizer.py
@@ -27,10 +27,41 @@ MAX_ITERS = 10000
 #--------------------------------------------------------------
 
 #------------------------- Functions --------------------------
-def Gradient_Ascent():
-    pass
+def gradient_ascent(f_xy, point: "numpy.ndarray"):
+    """ Perform the gradient ascent algorithm for MAX_ITERS iterations
+    until a local maximum is reached. Return the points that represent
+    the path the algorithm generated.
+    """
 
+    # Initialize the step, arrays of points and gradients
+    step = 0      
+    x_pts, y_pts, z_pts = [], [], []       
+    grad_p_x, grad_p_y = float("inf"), float("inf") 
 
+    while abs(float(grad_p_x)) > 0.001 and abs(float(grad_p_y)) > 0.001:
+            
+            # Find the gradient vector (i.e. the derivatives with respect to x and y)
+            f_xy_x, f_xy_y = sy.diff(f_xy, x), sy.diff(f_xy, y)               
+            
+            # Evaluate the gradient vector at point, p.
+            grad_p_x, grad_p_y = f_xy_x.subs([(x, point[0]), (y, point[1])]), f_xy_y.subs([(x, point[0]), (y, point[1])])
+
+            grad_point = np.array([float(grad_p_x), float(grad_p_y)])
+            
+            dir_vec = (1 / np.linalg.norm(grad_point)) * grad_point  # Find the direction vector of grad_point
+            
+            point += dir_vec * INCREASE  # Travel in the direction of dir_vec
+           
+            step += 1
+            
+            if step % 50 == 0:
+                x_pts.append(point[0])
+                y_pts.append(point[1])
+                z_pts.append(f_xy.subs([(x, point[0]), (y, point[1])]))
+                
+            if step > MAX_ITERS: break 
+
+    return x_pts, y_pts, z_pts
 #--------------------------------------------------------------
 
 if __name__ == "__main__":
@@ -56,47 +87,15 @@ if __name__ == "__main__":
  
 #--------------------------------------------------------------        
 
-#-------------  Gradient Ascent Optimization  -----------------
+#--------------  Gradient Ascent Optimization  ----------------
 
-        # 1 - Use the given point on the curve, p
         x_o, y_o = int(inp_x), int(inp_y)
-        point = np.array([x_o, y_o], dtype = float)
-        
-        
-        step = 0  # initialize step to help keep track of iterations 
-        
-        x_points, y_points, z_points = [], [], []  # initialize arrays to keep track of points
-        
-        grad_p_x, grad_p_y = float("inf"), float("inf")  # initialize grad_p_x and grad_p_y 
-        
-        
-        while abs(float(grad_p_x)) > 0.001 and abs(float(grad_p_y)) > 0.001:
-            
-            # 2 - Find the gradient vector (i.e. the derivatives with respect to x and y)
-            f_xy_x, f_xy_y = sy.diff(f_xy, x), sy.diff(f_xy, y) 
-        
-            # 3 - Evaluate the gradient vector at point, p.
-            grad_p_x, grad_p_y = f_xy_x.subs([(x, point[0]), (y, point[1])]), f_xy_y.subs([(x, point[0]), (y, point[1])])
+        point = np.array([x_o, y_o], dtype = float)        
+        x_pts, y_pts, z_pts = gradient_ascent(f_xy, point) # Perform the gradient ascent from the given point
 
-            grad_point = np.array([float(grad_p_x), float(grad_p_y)])
-            
-            # 4 - Find the direction vector of grad_point
-            dir_vec = (1 / np.linalg.norm(grad_point)) * grad_point
-            
-            # 5 - Travel in the direction of dir_vec
-            point += dir_vec * INCREASE
-           
-            step += 1
-            
-            if step % 50 == 0:
-                x_points.append(point[0])
-                y_points.append(point[1])
-                z_points.append(f_xy.subs([(x, point[0]), (y, point[1])]))
-                
-            if step > MAX_ITERS: break         
 #--------------------------------------------------------------
 
-#-------------------------- Graphing  -------------------------
+#------------------------- Graphing  --------------------------
 
         print("Function: {} | Initial Point: {} | Approximate Maximum: {}".format(f_xy, values, point))
         
@@ -117,7 +116,7 @@ if __name__ == "__main__":
         # Plot the line and the function
         fig = plt.figure()         
         ax = fig.add_subplot(1, 1, 1, projection = '3d')
-        ax.plot3D(x_points, y_points, z_points, 'red')
+        ax.plot3D(x_pts, y_pts, z_pts, 'red')
         #ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='magma', edgecolor='none')
         ax.plot_wireframe(X, Y, Z, rstride=1, cstride=1, color='black')
         ax.set_xlabel('x')
